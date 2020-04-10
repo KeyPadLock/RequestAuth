317eed89f25468db66985c0a3481e5182d1d9c74
collinw99 <collinw99@gmail.com>
2020-04-10 14:01:16 -0600

    Hashes passwd before sending to

--- sender.py
+++ sender.py
@@ -1,18 +1,40 @@
 # importing the requests library 
 import requests 
+import hashlib
+import time
 
-# api-endpoint 
-URL = "http://10.0.0.211:3000/auth"
+# validate password at API endpoint
+def validate(passwd, URL):
 
-passwd = "5407"
+	# hash the pin number
+	hashPass = hashlib.sha224(passwd.encode('utf8')).hexdigest()
 
-# defining a params dict for the parameters to be sent to the API 
-REQJSON = {'pass':passwd} 
+	# defining a params dict for the parameters to be sent to the API 
+	REQJSON = {'pass':hashPass} 
 
-# sending post request and saving the response as response object 
-r = requests.post(url = URL, data = REQJSON) 
+	# sending post request and saving the response as response object 
+	r = requests.post(url = URL, data = REQJSON) 
 
-# extracting data in json format 
-data = r.json() 
+	# extracting data in json format 
+	data = r.json() 
 
-print ("isValid: ", data['isValid'])
+	return data['isValid']
+
+if __name__ == '__main__':
+	MAXRETRIES = 4
+	tries = 0
+	while(True):
+		p = input()
+		valid = validate(p, "http://10.0.0.190:3000/auth")
+		if(valid):
+			print("Access Granted")
+			tries = 0
+		else:
+			print("Access Denied")
+			if(tries < MAXRETRIES):
+				tries += 1
+			else:
+				print("Timout for 30s")
+				tries = 0
+				time.sleep(30)
+			
